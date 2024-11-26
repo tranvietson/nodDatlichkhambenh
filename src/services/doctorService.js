0ce997c4dfa6a8d4ff3937e598fd65aa0017f4bf
TRAN VIET SON <tranvietson1983@gmail.com>
2024-11-26 21:33:58 +0700

    chi tiet chuyen khoa video 92

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -54,19 +54,44 @@ let getAllDoctorsService = () => {
     })
 }
 
+let checkRequiredFields = (inputData) => {
+    let arrFields = ['doctorId', 'contentHTML', 'contentMarkdown', 'action',
+        'selectedPrice', 'selectedPayment', 'selectedProvince', 'nameClinic',
+        'addressClinic', 'note', 'specialtyId'
+    ]
+    let isValid = true;
+    let element = '';
+    for (let i = 0; i < arrFields.length; i++) {
+        if (!inputData[arrFields[i]]) {
+            isValid = false;
+            element = arrFields[i];
+            break;
+        }
+    }
+
+    return {
+        isValid: isValid,
+        element: element
+    }
+}
+
 let saveDetailInforDortorService = (inputData) => {
     console.log('>>>>>>>>>>gia tri cua inputData: ', inputData, typeof inputData)
     return new Promise(async (resolve, reject) => {
         try {
-            if (!inputData.doctorId || !inputData.contentHTML
-                || !inputData.contentMarkdown || !inputData.action
-                || !inputData.selectedPrice || !inputData.selectedPayment
-                || !inputData.selectedProvince || !inputData.nameClinic
-                || !inputData.addressClinic || !inputData.note
-            ) {
+            let checkObj = checkRequiredFields(inputData);
+            if (checkObj.isValid === false)
+            // !inputData.doctorId || !inputData.contentHTML
+            // || !inputData.contentMarkdown || !inputData.action
+            // || !inputData.selectedPrice || !inputData.selectedPayment
+            // || !inputData.selectedProvince || !inputData.nameClinic
+            // || !inputData.addressClinic || !inputData.note
+            // || !inputData.specialtyId
+
+            {
                 resolve({
                     errCode: 1,
-                    errMessage: 'Missing parameter'
+                    errMessage: `Missing parameter: ${checkObj.element}`
                 })
             } else {
                 // upsert to Markdown
@@ -109,6 +134,8 @@ let saveDetailInforDortorService = (inputData) => {
                     doctorInfor.nameClinic = inputData.nameClinic;
                     doctorInfor.addressClinic = inputData.addressClinic;
                     doctorInfor.note = inputData.addressClinic;
+                    doctorInfor.specialtyId = inputData.specialtyId;
+                    doctorInfor.clinicId = inputData.clinicId;
                     await doctorInfor.save()
                 } else {
                     // create
@@ -120,6 +147,8 @@ let saveDetailInforDortorService = (inputData) => {
                         nameClinic: inputData.nameClinic,
                         addressClinic: inputData.addressClinic,
                         note: inputData.note,
+                        specialtyId: inputData.specialtyId,
+                        clinicId: inputData.clinicId
 
                     })
                 }
